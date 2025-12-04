ae8ad9d1a837075466eb3a483a63b1a2f3ba7752
Mike <tankf33der@disroot.org>
2025-12-04 11:53:10 +0200

    .

--- almost_test.v
+++ almost_test.v
@@ -1,18 +1,14 @@
 module almost
 
-import tankf33der.almost
-
 fn test_f64_eq_almost() {
 	mut a := 3.141592653589793
 	mut b := 3.141592653589790
 	assert almost.equal(a, b)
-/*
 	a = 3.141592653589793
 	b = 3.141592653589700
-	assert a.almost.eq(b, absolute: 1e-10)
+	assert almost.equal(a, b, absolute: 1e-10)
 	a = 1e-20
 	b = 2e-20
-	assert a.almost.eq(b)
-	assert a.almost.eq(b, absolute: 0, relative: 0) == false
-*/
+	assert almost.equal(a, b)
+	assert almost.equal(a, b, absolute: 0, relative: 0) == false
 }
